de6ec5f7a760f4f7384cd39918881fbc9fa67dd2
YuDe <davisfreeman1015@gmail.com>
2017-03-27 15:40:07 +0800

    New progress of homework

--- hw3.R
+++ hw3.R
@@ -5,7 +5,11 @@ ptt_movie = 'https://www.ptt.cc/bbs/movie/index'
 html_tail = '.html'
 pages = seq(from=1, to=1000, by=2)
 
-for(page in pages) {
+authors <- c() 
+titles  <- c()
+numbers <- c()
+
+for(page in pages[1:10]) {
   
   url <-paste0(ptt_movie, toString(page), '.html')
   
@@ -17,7 +21,21 @@ for(page in pages) {
     title  <- post%>%html_nodes('.title a')%>%html_text()
     number <- post%>%html_nodes('.nrec span')%>%html_text()
     
+    if (identical(number, character(0))) {
+      numbers <- c(numbers, '0')
+    } else {
+      numbers <- c(numbers, number)
+    }
+    
+    authors <- c(authors, author)
+    titles  <- c(titles, title)
+    
     
   }
 }
 
+print(authors)
+print(titles)
+print(numbers)
+
+data_output <- data.frame(Authors=authors, Titles=titles, Numbers=numbers)
